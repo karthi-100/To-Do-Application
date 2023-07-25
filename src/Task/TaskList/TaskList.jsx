2abac14340e2be1c322d91ac1a61bd4846506d41
karthi-100 <karthivicky42@gmail.com>
2023-07-26 01:31:55 +0530

    commit

---
+++ src/Task/TaskList/TaskList.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {MdDelete} from '../../../node_modules/react-icons/md'
+
+import "./TaskList.css"
+const TaskList=(prop)=>{
+const [status,setStatus]=useState("Un Done")
+const changeCheck=(e)=>{
+if(e.target.checked){
+    setStatus("Done")
+}
+else
+    setStatus("Un Done")
+}
+const handleDelete=()=>{
+    prop.toDelete(prop.id)
+}
+    return(
+        <div className="taskList">
+            <div className="t-left">
+            <div className="t-name">{prop.name.charAt(0).toUpperCase()+prop.name.slice(1)}</div>
+            <div className="t-date">Date: {prop.date}/{prop.month}/{prop.year}</div>
+            <div className="t-status">Status: {status}</div>
+            </div>
+            <div className="t-right">
+            <div>
+            <input type="checkbox" onClick={changeCheck} /></div>
+            
+            <button className="btndelete" onClick={handleDelete}><MdDelete className="deletebtn"/></button>   
+            </div>
+        </div>
+    )
+}
+export default TaskList
